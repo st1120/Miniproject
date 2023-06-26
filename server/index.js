bb6d74a7b2f36b3963bb29864b90ef05a07c9099
Your Name <you@example.com>
2023-06-27 01:27:41 +0530

    Upload Files

--- server/index.js
+++ server/index.js
@@ -5,19 +5,18 @@ const bodyParser = require("body-parser");
 const app = express();
 const url = "mongodb+srv://admin:admin@ambulancedispatch.7yt2txc.mongodb.net/";
 
-
 const userRouter = require("./routes/Users");
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.once("open", ()=> {
-    console.log("connected");
+    console.log("Connected");
 })
 
 //middlewares
 app.use("/auth", userRouter);
-app.use(cors());
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.listen("3001",()=> {
     
